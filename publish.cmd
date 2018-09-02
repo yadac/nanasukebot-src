e09c518fc0578d402ec7a0cdff5b3211e487165b
yosuke.adachi <yosuke.adachi@avanade.com>
2018-09-02 16:51:30 +0900

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=nanasukebot-Web-Deploy.pubxml -p:Password=sYlfZiiyPqSPx8aRqfT0Eam3yDQxn2wykEDNnm31NXuflEPKS0QFNQxFq2og
+
